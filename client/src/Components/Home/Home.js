afd0bdfa5f0a4e7bc568d8c3575e2bf8af0dd210
Hinduraj8199 <hindurajkkadam123@gmail.com>
2021-08-08 14:30:28 +0530

    Css Modified

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -3,7 +3,17 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h2>Home</h2>
+      <h2
+        style={{
+          color: "#fc6203",
+          position: "absolute",
+          fontSize: "100px",
+          top: 150,
+          left: 350,
+        }}
+      >
+        Admin Dashboard
+      </h2>
     </div>
   );
 };
